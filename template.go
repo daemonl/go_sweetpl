3e79b1bf511c4146274158d211399a0497eeb8cb
Damien Whitten <damien@rebase.com.au>
2014-06-16 08:07:49 +1000

    Light non existing checking, improved matching for define and template tags

--- template.go
+++ template.go
@@ -4,12 +4,13 @@ import (
 	"fmt"
 	"html/template"
 	"io"
+
 	"regexp"
 )
 
 var re_extends *regexp.Regexp = regexp.MustCompile("{{ extends [\"']?([^'\"}']*)[\"']? }}")
-var re_defineTag *regexp.Regexp = regexp.MustCompile("{{ ?define \"([^\"]*)\" ?}}")
-var re_templateTag *regexp.Regexp = regexp.MustCompile("{{ ?template \"([^\"]*)\" ([^ ]*)? ?}}")
+var re_defineTag *regexp.Regexp = regexp.MustCompile("{{ ?define \"([^\"]*)\" ?\"?([a-zA-Z0-9]*)?\"? ?}}")
+var re_templateTag *regexp.Regexp = regexp.MustCompile("{{ ?template \"([^\"]*)\" ?([^ ]*)? ?}}")
 
 //var re_endableTag *regexp.Regexp = regexp.MustCompile(`{{ ?(define|if|range|with|end)[^}]*? ?}}`)
 
@@ -27,6 +28,10 @@ func (st *SweeTpl) Render(w io.Writer, name string, data interface{}) error {
 	tpl, err := st.assemble(name)
 	if err != nil {
 		return err
+	}
+
+	if tpl == nil {
+		return Errf("Nil template named %s", name)
 	}
 
 	err = tpl.Execute(w, data)
@@ -47,9 +52,16 @@ func (st *SweeTpl) add(stack *[]*NamedTemplate, name string) error {
 		return err
 	}
 
+	if len(tplSrc) < 1 {
+		return Errf("Empty Template named %s", name)
+	}
+
 	extendsMatches := re_extends.FindStringSubmatch(tplSrc)
 	if len(extendsMatches) == 2 { //Did Match
-		st.add(stack, extendsMatches[1])
+		err := st.add(stack, extendsMatches[1])
+		if err != nil {
+			return err
+		}
 		tplSrc = re_extends.ReplaceAllString(tplSrc, "")
 	}
 	namedTemplate := &NamedTemplate{
@@ -65,7 +77,10 @@ func (st *SweeTpl) assemble(name string) (*template.Template, error) {
 
 	stack := []*NamedTemplate{}
 
-	st.add(&stack, name)
+	err := st.add(&stack, name)
+	if err != nil {
+		return nil, err
+	}
 
 	// The stack is ordered 'general' to 'specific'
 
@@ -83,6 +98,7 @@ func (st *SweeTpl) assemble(name string) (*template.Template, error) {
 			parsed := re_defineTag.FindStringSubmatch(raw)
 			blockName := fmt.Sprintf("BLOCK_%d", blockId)
 			blocks[parsed[1]] = blockName
+
 			blockId += 1
 			return "{{ define \"" + blockName + "\" }}"
 		})
@@ -98,7 +114,7 @@ func (st *SweeTpl) assemble(name string) (*template.Template, error) {
 
 			// Default the import var to . if not set
 			dot := "."
-			if len(parsed) == 3 {
+			if len(parsed) == 3 && len(parsed[2]) > 0 {
 				dot = parsed[2]
 			}
 			if ok {
